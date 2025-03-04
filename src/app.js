46d1a607dc01f531dad31798b1e5962f9bf310e1
timbermonson <timbermonson@gmail.com>
2025-03-04 13:26:13 -0700

    Delete old app.js

--- src/app.js
+++
@@ -1,9 +0,0 @@
-import getSaltLakeCountyResultPageByName from './GetSLCAddress.js'
-
-const args = process.argv
-args.splice(0, 2)
-
-const nameInput = args.join(' ')
-const resp = await getSaltLakeCountyResultPageByName(nameInput)
-
-console.log(JSON.stringify(resp, null, 2))
