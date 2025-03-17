ca558d7f904d833dde63350da0dc259ff400faf4
timbermonson <timbermonson@gmail.com>
2025-03-17 11:54:52 -0600

    Add Iron County, Sevier County, Sanpete County

--- src/apps/ScrapeAddresses/countyPlugins/index.js
+++ src/apps/ScrapeAddresses/countyPlugins/index.js
@@ -1,3 +1,6 @@
+import IronCountySearch from './IronCounty.js'
+import SanpeteCountySearch from './SanpeteCounty.js'
+import SevierCountySearch from './SevierCounty.js'
 import SLCountySearch from './SLCounty.js'
 import SummitCountySearch from './SummitCounty.js'
 import TooeleCountySearch from './TooeleCounty.js'
@@ -6,7 +9,10 @@ import WashingtonCountySearch from './WashCounty.js'
 import WeberCountySearch from './WeberCounty.js'
 
 export default {
+    'Iron County': IronCountySearch,
     'Salt Lake': SLCountySearch,
+    'Sanpete County': SanpeteCountySearch,
+    'Sevier County': SevierCountySearch,
     'Summit County': SummitCountySearch,
     'Tooele County': TooeleCountySearch,
     'Utah County': UTCountySearch,
