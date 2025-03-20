1b50e345f088dbf870eb699a6d4095df70ad4414
timbermonson <timbermonson@gmail.com>
2025-03-20 16:24:55 -0600

    Adjust testrunner settings

--- src/vitest.config.js
+++ src/vitest.config.js
@@ -2,7 +2,10 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
     test: {
-        include: ['**/*.test.js'],
+        coverage: {
+            clean: true,
+        },
         globals: true,
+        include: ['**/*.test.js'],
     },
 })
