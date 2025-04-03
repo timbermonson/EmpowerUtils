546a4eb77fbb275aba05ab700cebdb6867548b62
timbermonson <timbermonson@gmail.com>
2025-04-03 16:23:38 -0600

    Refactor xero importer, create xero lib

--- src/apps/XeroImport/app.js
+++ src/apps/XeroImport/app.js
@@ -12,15 +12,13 @@ const {
 } = lib.io
 
 const { setupWebsocket } = lib.browser
+const { Xero } = lib.Xero
 
 import { compact } from 'lodash-es'
 
 const debugPort = 9222
 
 async function finish(ws) {
-    // await inquirerConfirm({
-    //     message: 'Submit any reponse to close.',
-    // })
     await ws.close()
 }
 
@@ -44,61 +42,14 @@ function getInputLine(lineNum) {
     return inputLines[lineNum]
 }
 
-const wait = (time) => new Promise((resolve) => setTimeout(resolve, time))
-
-async function switchToOrg(ws, orgName) {
-    // const dashOpAccBtn =
-    //     "$('div.mf-bank-widget-panel').has('h2:contains(\"Operating:\"):contains(\"2894\")).find('.mf-bank-widget-touchtarget')"
-
-    const { j, w, findAndDo: f } = ws
-
-    // Get the menu dropdown open & reset
-    await f(
-        'j{.xnav-appbutton}.n{.xnav-appbutton-is-active}',
-        async (q) => await j(`${q}.click()`),
-        'j{.xnav-orgsearchcontainer:has(button.xnav-icon-orgsearchclear)}',
-        async (q) => await j(`${q}.f{button.xnav-icon-orgsearchclear}.click()`),
-        'j{.xnav-appbutton.xnav-appbutton-is-active}',
-        () => {}
-    )
-
-    // Type name, select, wait for pageload
-    await j('j{[data-name="xnav-changeorgbutton"]}.click()')
-
-    await w('j{.xnav-orgsearch--input}')
-    await j(
-        `j{input.xnav-orgsearch--input}.g{0}.value = ${JSON.stringify(orgName)}`
-    )
-    await j(
-        `j{input.xnav-orgsearch--input}.g{0}.dispatchEvent(new KeyboardEvent("keyup"))`
-    )
-    const orgSearchFirst =
-        'ol[role="navigation"].xnav-verticalmenu > li:nth-child(1) > a'
-    await wait(500)
-    await f(`j{${orgSearchFirst}}`, async (q) => await j(`${q}.g{0}.click()`))
-    await ws.waitLoad()
-
-    // Nav to imports
-    await f(
-        'j{.mf-bank-widget-panel:contains("Operating"):contains("2894")>div>div>button}',
-        async (q) => await j(`${q}.click()`)
-    )
-    await f(
-        'j{a:contains("Import a Statement")}',
-        async (q) => await j(`${q}.g{0}.click()`)
-    )
-}
-
-async function openImports(ws, orgName) {
-    await switchToOrg(ws, orgName)
-}
-
 async function run() {
     init()
 
     let ws = await setupWebsocket(debugPort, ({ url }) =>
         url.includes('go.xero.com/app/')
     )
+
+    const xero = new Xero(ws)
 
     let curLineNum = 0
     while (true) {
@@ -113,7 +64,8 @@ async function run() {
         appendOutputData(curLine + '\n')
         if (await confirm(`Open imports: [${curLine}]?`)) {
             try {
-                await openImports(ws, curLine)
+                await xero.switchToOrg(curLine)
+                await xero.navToImports()
             } catch (e) {
                 console.error(e)
             }
--- src/lib/index.js
+++ src/lib/index.js
@@ -2,6 +2,7 @@ import * as address from './address.js'
 import * as browser from './browser.js'
 import * as io from './io.js'
 import * as scraper from './scraper.js'
+import * as Xero from './Xero.js'
 import * as str from './string.js'
 
-export default { io, address, scraper, str, browser }
+export default { io, address, scraper, str, browser, Xero }
