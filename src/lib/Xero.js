03a898ef28d00e84fc676f0f06ccef142e2c67d1
timbermonson <timbermonson@gmail.com>
2025-04-07 14:36:32 -0600

    Finalize xero aged-check-getting

--- src/lib/Xero.js
+++ src/lib/Xero.js
@@ -1,8 +1,9 @@
+import dayjs from 'dayjs'
 import { wait } from './etc.js'
 import { lm } from './io.js'
 
 const operatingButton =
-    'j{.mf-bank-widget-panel:contains("Operating"):contains("2894")>div>div>button}'
+    'j{.mf-bank-widget-panel:contains("Operating")>div>div>button}'
 
 export default class Xero {
     #autoBrowser
@@ -11,8 +12,10 @@ export default class Xero {
     }
 
     async switchToOrg(orgName) {
-        lm(`Switching to ${orgName}...`)
+        lm(`• Switching to ${orgName}...`)
         const { autoBrowser: ab } = this
+        await ab.cons('location.reload()')
+        await ab.waitPageLoad()
 
         const orgChangeBtn = 'j{[data-name="xnav-changeorgbutton"]}'
 
@@ -32,40 +35,62 @@ export default class Xero {
         await ab.click(
             `j{ol[role="navigation"].xnav-verticalmenu > li:nth-child(1) > a}`
         )
-
         await ab.waitPageLoad()
+
         await ab.w(operatingButton)
-        lm('Done!')
+        lm('○ Done!')
     }
 
     async navToImports() {
-        lm('Navigating to imports...')
+        lm('• Navigating to imports...')
         const { autoBrowser: ab } = this
         await ab.click(operatingButton)
         await ab.click('j{a:contains("Import a Statement")}')
         await ab.w(
             'j{.xui-pageheading--titlewrapper:contains("Import bank transactions")}'
         )
-        lm('Done!')
+        lm('○ Done!')
     }
 
     async openAgedChecks() {
-        lm('Opening aged checks...')
-
         const { autoBrowser: ab } = this
+        lm('• Opening aged checks...')
+
         await ab.click(operatingButton)
         await ab.click(
             'j{.mf-bank-widget-text-minorlink:contains("Account Transactions")}'
         )
+
         await ab.waitPageLoad()
         await ab.w('j{#removeAndRedoButton}')
+
         await ab.cons('window.Bank.toggleSearchForm();')
         await ab.w('j{.search.action.open}')
-        await ab.type('j{#sb_reconciledStatus_value}', 'Unreconciled')
+
+        await ab.cons('jQuery.noConflict()')
+        await ab.cons('jQuery.noConflict()')
+        await ab.type('j{#sb_reconciledStatus_value}', 'Un')
         await ab.click(
             'j{#sb_reconciledStatus_suggestions>div>div:contains("Unreconciled")}'
         )
 
-        lm('Done!')
+        await ab.type(
+            'j{#sb_dteEndDate}',
+            `${dayjs().subtract(90, 'day').format('MMM D, YYYY')}`
+        )
+
+        await ab.type('j{#sb_reconciledStatus_value}', 'Un')
+        await ab.cons('jQuery.noConflict()')
+        await ab.type('j{#sb_reconciledStatus_value}', 'Un')
+        await wait(500)
+        await ab.click(
+            'j{#sb_reconciledStatus_suggestions>div>div:contains("Unreconciled")}'
+        )
+
+        await ab.click('j{#sbSubmit_BT}')
+        await ab.waitPageLoad()
+        await ab.w('j{#bankTransactions}')
+
+        await lm('○ Done!')
     }
 }
