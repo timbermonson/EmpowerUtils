3faa2f73a7019000fc4921ee6ed7e428a136219f
timbermonson <timbermonson@gmail.com>
2025-04-17 10:25:02 -0600

    add fn: doWhileUndefined + TimeoutError class

--- src/lib/etc.js
+++ src/lib/etc.js
@@ -1,3 +1,31 @@
+class TimeoutError extends Error {
+    constructor(message, options) {
+        super(message, options)
+        this.name = 'TimeoutError'
+    }
+}
+
 const wait = (time) => new Promise((resolve) => setTimeout(resolve, time))
 
-export { wait }
+const doWhileUndefined = async (timeout, interval, callback) => {
+    const startTime = Date.now()
+    let millisPassed = 0
+    let attemptNum = 0
+
+    while (millisPassed < timeout) {
+        const returnValue = await callback(millisPassed, attemptNum)
+
+        if (returnValue !== undefined) {
+            return returnValue
+        } else {
+            await wait(interval)
+        }
+
+        attemptNum += 1
+        millisPassed = Date.now() - startTime
+    }
+
+    throw new TimeoutError()
+}
+
+export { doWhileUndefined, TimeoutError, wait }
